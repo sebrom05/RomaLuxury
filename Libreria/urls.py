c97ea3b04cb54b773b50d5896e576b5bfe574d6e
Sebastian Romero <sebrom05@gmail.com>
2024-11-13 11:19:25 -0500

    Nuevos cambios

--- Libreria/urls.py
+++ Libreria/urls.py
@@ -14,27 +14,32 @@ urlpatterns = [
     path('Categorias/EditarCategoria/<int:id>', views.EditarCategoria, name='EditarCategoria'),
     path('Categorias/BorrarCategoria/<int:id>', views.BorrarCategoria, name='BorrarCategoria'),
 
-    path('Productos', views.Productos, name='Productos'),
+    path('Productos', views.Productos_1, name='Productos'),
     path('Productos/CrearProducto', views.CrearProducto, name='CrearProducto'),
-    path('Categorias/EditarProducto', views.EditarProducto, name='EditarProducto'),
+    path('Categorias/EditarProducto/<int:id>', views.EditarProducto, name='EditarProducto'),
+    path('Categorias/EliminarProducto/<int:id>', views.EliminarProducto, name='EliminarProducto'),
 
 
-    path('Clientes', views.Clientes, name='Clientes'),
+    path('Clientes', views.Clientes_1, name='Clientes'),
     path('Clientes/CrearCliente', views.CrearCliente, name='CrearCliente'),
-    path('Clientes/EditarCliente', views.EditarCliente, name='EditarCliente'),
+    path('Clientes/EditarCliente/<int:id>', views.EditarCliente, name='EditarCliente'),
+    path('Clientes/BorrarCliente/<int:id>', views.BorrarCliente, name='BorrarCliente'),
 
 
-    path('Empleados', views.Empleados, name='Empleados'),
+    path('Empleados', views.Empleados_1, name='Empleados'),
     path('Empleados/CrearEmpleado', views.CrearEmpleado, name='CrearEmpleado'),
-    path('Empleados/EditarEmpleado', views.EditarEmpleado, name='EditarEmpleado'),
+    path('Empleados/EditarEmpleado/<int:id>', views.EditarEmpleado, name='EditarEmpleado'),
+    path('Empleados/EliminarEmpleado/<int:id>', views.EliminarEmpleado, name='EliminarEmpleado'),
 
-    path('FacturaPagos', views.FacturaPagos, name='FacturaPagos'),
+    path('FacturaPagos', views.FacturaPagos_1, name='FacturaPagos'),
     path('FacturaPagos/CrearPago', views.CrearPago, name='CrearPago'),
-    path('FacturaPagos/EditarPago', views.EditarPago, name='EditarPago'),
+    path('FacturaPagos/EditarPago/<int:id>', views.EditarPago, name='EditarPago'),
+    path('FacturaPagos/BorrarPago/<int:id>', views.BorrarPago, name='BorrarPago'),
 
-    path('MetodoPago', views.MetodoPago, name='MetodoPago'),
+    path('MetodoPago', views.MetodoPago_1, name='MetodoPago'),
     path('MetodoPago/AñadirMetodoPago', views.AñadirMetodoPago, name='AñadirMetodoPago'),
-    path('MetodoPago/EditarMetodoPago', views.EditarMetodoPago, name='EditarMetodoPago'),
+    path('MetodoPago/EditarMetodoPago/<int:id>', views.EditarMetodoPago, name='EditarMetodoPago'),
+    path('MetodoPago/BorrarMetodoPago/<int:id>', views.BorrarMetodoPago, name='BorrarMetodoPago'),
 
 
 ]
--- Libreria/views.py
+++ Libreria/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from Libreria.models import Categorias
+from Libreria.models import Categorias, Clientes, Productos, Empleados, FacturaPagos, MetodoPago
 from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
@@ -33,55 +33,79 @@ def BorrarCategoria(request,id):
 
 
 
-def Productos(request):
-    return render(request,'Productos/Productos.html' )
+def Productos_1(request):
+    productos = Productos.objects.all()
+    return render(request,'Productos/Productos.html', {'productos': productos} )
 
 def CrearProducto(request):
     return render(request,'Productos/CrearProducto.html' )
 
-def EditarProducto(request):
-    return render(request,'Productos/EditarProducto.html' )
+def EditarProducto(request,id):
+    print(id, "Este es el id")
+    return render(request,'Productos/EditarProducto.html', {'id': id} )
+
+def EliminarProducto(request,id):
+    return render(request, 'Productos/EliminarProducto.html')
 
 
 
-def Clientes(request):
-    return render(request,'Clientes/Clientes.html' )
+def Clientes_1(request):
+    clientes = Clientes.objects.all()  # Obtiene todas las categorías
+    return render(request,'Clientes/Clientes.html', {'clientes': clientes} )
 
 def CrearCliente(request):
     return render(request,'Clientes/CrearCliente.html' )
 
-def EditarCliente(request):
-    return render(request,'Clientes/EditarCliente.html' )
+def EditarCliente(request,id):
+    print(id, "Este es el id")
+    return render(request,'Clientes/EditarCliente.html', {'id': id})
+
+def BorrarCliente(request,id):
+    return render(request, 'Clientes/BorrarCliente.html')
 
 
 
-def Empleados(request):
-    return render(request,'Empleados/Empleados.html' )
+def Empleados_1(request):
+    empleados = Empleados.objects.all()
+    return render(request,'Empleados/Empleados.html', {'empleados': empleados} )
 
 def CrearEmpleado(request):
     return render(request,'Empleados/CrearEmpleado.html' )
 
-def EditarEmpleado(request):
-    return render(request,'Empleados/EditarEmpleado.html' )
+def EditarEmpleado(request,id):
+    print(id, "Este es el id")
+    return render(request,'Empleados/EditarEmpleado.html', {'id': id} )
+
+def EliminarEmpleado(request,id):
+    return render(request, 'Empleados/EliminarEmpleado.html')
 
 
 
-def FacturaPagos(request):
-    return render(request,'FacturaPagos/FacturaPagos.html' )
+def FacturaPagos_1(request):
+    facturapagos = FacturaPagos.objects.all()
+    return render(request,'FacturaPagos/FacturaPagos.html', {'facturapagos' : facturapagos} )
 
 def CrearPago(request):
     return render(request,'FacturaPagos/CrearPago.html' )
 
-def EditarPago(request):
-    return render(request,'FacturaPagos/EditarPago.html' )
+def EditarPago(request,id):
+    print(id, "Este es el id")
+    return render(request,'FacturaPagos/EditarPago.html', {'id': id} )
+
+def BorrarPago(request,id):
+    return render(request,'FacturaPagos/BorrarPago.html' )
 
 
-
-def MetodoPago(request):
-    return render(request,'MetodoPago/MetodoPago.html' )
+def MetodoPago_1(request):
+    metodopagos =MetodoPago.objects.all()
+    return render(request,'MetodoPago/MetodoPago.html', {'metodopagos' : metodopagos} )
 
 def AñadirMetodoPago(request):
     return render(request,'MetodoPago/AñadirMetodoPago.html' )
 
-def EditarMetodoPago(request):
-    return render(request,'MetodoPago/EditarMetodoPago.html' )
+def EditarMetodoPago(request,id):
+    print(id, "Este es el id")
+    return render(request,'MetodoPago/EditarMetodoPago.html', {'id': id} )
+
+def BorrarMetodoPago(request):
+    return render(request,'MetodoPago/BorrarMetodoPago.html' )
